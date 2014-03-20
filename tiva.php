7f6c121406135354df2bd866a099d8014fe36040
sm65 <sm65@users.noreply.github.com>
2014-03-20 12:05:20 -0400

    Update tiva.php

--- tiva.php
+++ tiva.php
@@ -1,1 +1,20 @@
-x
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<meta name="description" content="" />
+<meta name="keywords" content="" />
+<meta name="author" content="" />
+<link rel="stylesheet" type="text/css" href="style.css" media="screen" />
+
+<title>TI Tiva C TM4C1294 Connected LaunchPad</title>
+</head>
+<body>
+<div id="wrapper">
+<?php include('includes/header.php'); ?>
+<?php include('includes/nav.php'); ?>
+<?php include('includes/sidebar.php'); ?>
+<iframe id='tiva-content' src='tiva.php' frameborder='0' </iframe>
+</div> <!-- End #wrapper -->
+</body>
+</html>
