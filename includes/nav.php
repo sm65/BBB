9f7641f13623e721ec0a38fab5b429e002690445
sm65 <sm65@users.noreply.github.com>
2016-01-05 04:58:21 -0500

    Update nav.php

--- includes/nav.php
+++ includes/nav.php
@@ -1,8 +1,8 @@
 <div id="nav">
 
 	<a href="index.php">Home</a>
+	<a href="graph.php">O2 graph</a>
 	<a href="O2.php#">O2 Log</a>
 	<a href="about.php">BBB Stuff</a>
-	<a href="tiva.php">TI Tiva C</a>
 	<!-- <a href="chart.php">Google Chart</a> -->
 </div> <!-- end #nav -->
