af40c4e081c5fdbb15d7040598bd639186dd2c61
sm65 <sm65@users.noreply.github.com>
2016-01-05 06:41:08 -0500

    Update sidebar.php

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,7 +1,8 @@
 <div id="sidebar">
 
-<h3>Links</h3>
+<h3>Links</h3>http:
         <li><a href="http://www.wunderground.com/US/NC/Indian_Trail.html?extendedsun=sunon&MR=1">WunderGround IT</a></li>
+        <li><a href="http://rd.excite.com/sp/em">Email</a></li>
 	<li><a href="http://beagleboard.org/Products/BeagleBone%20Black">BBB Mfg. page</a></li>
 	<li><a href="http://elinux.org/BeagleBoardUbuntu#eMMC:_BeagleBone_Black">BBB eLinux page</a></li>
 	<li><a href="https://github.com/sm65/BBB">Source code page</a></li>
