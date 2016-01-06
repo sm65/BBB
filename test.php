3fd92b899ec9706d740ac45321f34030fdbe6d31
sm65 <sm65@users.noreply.github.com>
2016-01-05 19:26:19 -0500

    Update test.php

--- test.php
+++ test.php
@@ -1,30 +1,40 @@
-<html>
-  <head>
-    <script type="text/javascript" src="https://www.google.com/jsapi"></script>
-    <script type="text/javascript">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+  <title>Google AJAX Feed API - AJAX Slide Show Sample</title>
+  <script src="https://www.google.com/jsapi/?key=YOUR-KEY"
+    type="text/javascript"></script>
+  <script src="http://www.google.com/uds/solutions/dynamicfeed/gfdynamicfeedcontrol.js"
+    type="text/javascript"></script>
 
-    google.load("feeds", "1");
+  <style type="text/css">
+    @import url("http://www.google.com/uds/solutions/dynamicfeed/gfdynamicfeedcontrol.css");
 
-    function initialize() {
-      var feed = new google.feeds.Feed("http://feeds.reuters.com/Reuters/worldNews");
-      feed.setResultFormat(google.feeds.Feed.XML_FORMAT);
-      feed.load(function(result) {
-        if (!result.error) {
-          var container = document.getElementById("feed");
-          for (var i = 0; i < result.feed.entries.length; i++) {
-            var entry = result.feed.entries[i];
-            var div = document.createElement("div");
-            div.appendChild(document.createTextNode(entry.title));
-            container.appendChild(div);
-          }
-        }
-      });
+    #feedControl { 
+      margin-top : 20px;
+      margin-left: auto;
+      margin-right: auto;
+      width : 440px;
+      font-size: 16px;
+      color: #9CADD0;
     }
-    google.setOnLoadCallback(initialize);
+  </style>
+  <script type="text/javascript">
+    function load() {
+      var feed ="http://rss.people.com/web/people/rss/topheadlines/index.xml";
+      new GFdynamicFeedControl(feed, "feedControl");
 
-    </script>
-  </head>
-  <body>
-    <div id="feed"></div>
-  </body>
+    }
+    google.load("feeds", "1");
+    google.setOnLoadCallback(load);
+  </script>
+</head>
+
+<body>
+  <div id="body">
+    <div id="feedControl">Loading...</div>
+  </div>
+</body>
 </html>
