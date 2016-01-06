ee780c119a7cf7de1f6d5394a067c3e452c5128e
sm65 <sm65@users.noreply.github.com>
2016-01-05 19:35:41 -0500

    Update test.php

--- test.php
+++ test.php
@@ -1,40 +1,69 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-  <title>Google AJAX Feed API - AJAX Slide Show Sample</title>
-  <script src="https://www.google.com/jsapi/?key=YOUR-KEY"
-    type="text/javascript"></script>
-  <script src="http://www.google.com/uds/solutions/dynamicfeed/gfdynamicfeedcontrol.js"
-    type="text/javascript"></script>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<meta name="description" content="" />
+<meta name="keywords" content="" />
+<meta name="author" content="" />
+<link rel="stylesheet" type="text/css" href="style.css" media="screen" />
+<title>Home Page</title>
+<script type="text/javascript" src="https://www.google.com/jsapi"></script>
+<script src="http://www.google.com/uds/solutions/dynamicfeed/gfdynamicfeedcontrol.js"
+type="text/javascript"></script>
 
-  <style type="text/css">
-    @import url("http://www.google.com/uds/solutions/dynamicfeed/gfdynamicfeedcontrol.css");
+<style type="text/css">
+@import url("http://www.google.com/uds/solutions/dynamicfeed/gfdynamicfeedcontrol.css");
+#feedControlBBC {
+margin-top : 10px;
+margin-left: 10px;
+margin-right: auto;
+width : 440px;
+font-size: 12px;
+color: #9CADD0;
+}
+#feedControlRTR {
+margin-top : 10px;
+margin-left: 10px;
+margin-right: auto;
+width : 440px;
+font-size: 12px;
+color: #9CADD0;
+}
+</style>
 
-    #feedControl { 
-      margin-top : 20px;
-      margin-left: auto;
-      margin-right: auto;
-      width : 440px;
-      font-size: 16px;
-      color: #9CADD0;
-    }
-  </style>
-  <script type="text/javascript">
-    function load() {
-      var feed ="http://rss.people.com/web/people/rss/topheadlines/index.xml";
-      new GFdynamicFeedControl(feed, "feedControl");
-
-    }
-    google.load("feeds", "1");
-    google.setOnLoadCallback(load);
-  </script>
+<script type="text/javascript">
+function load() {
+var feed ="http://feeds.bbci.co.uk/news/world/rss.xml";
+new GFdynamicFeedControl(feed, "feedControlBBC");
+}
+google.load("feeds", "1");
+google.setOnLoadCallback(load);
+</script>
+<script type="text/javascript">
+function load() {
+var feed ="http://feeds.reuters.com/Reuters/worldNews";
+  var options = {
+    numResults : 6,
+    displayTime : 0
+  }
+new GFdynamicFeedControl(feed, "feedControlRTR", options);
+}
+google.load("feeds", "1");
+google.setOnLoadCallback(load);
+</script>
 </head>
-
-<body>
-  <div id="body">
-    <div id="feedControl">Loading...</div>
-  </div>
-</body>
+        <body>
+                <div id="wrapper">
+<?php include('includes/header.php');
+	include('includes/nav.php');
+	include('includes/sidebar.php');
+?>
+<div id="body">
+<div id="feedControlBBC">Loading BBC...</div>
+</div>
+<div id="body">
+<div id="feedControlRTR">Loading Reuters...</div>
+</div>
+                </div> <!-- End #wrapper -->
+        </body>
 </html>
