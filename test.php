1e9c5c0a50e06beb93cd7c01f76a565554193c64
sm65 <sm65@users.noreply.github.com>
2016-04-14 05:39:10 -0400

    Update test.php

--- test.php
+++ test.php
@@ -11,7 +11,24 @@
   <h1>UDP server test</h1> 
 
 <?php
+/* Open a server socket to port 1234 on localhost */
+$server = stream_socket_server('tcp://127.0.0.1:5001');
 
+/* Accept a connection */
+$socket = stream_socket_accept($server);
+
+/* Grab a packet (1500 is a typical MTU size) of OOB data */
+echo "Received Out-Of-Band: '" . stream_socket_recvfrom($socket, 14, STREAM_OOB) . "'\n";
+
+/* Take a peek at the normal in-band data, but don't comsume it. */
+echo "Data: '" . stream_socket_recvfrom($socket, 14, STREAM_PEEK) . "'\n";
+
+/* Get the exact same packet again, but remove it from the buffer this time. */
+echo "Data: '" . stream_socket_recvfrom($socket, 14) . "'\n";
+
+/* Close it up */
+fclose($socket);
+fclose($server);
 ?>
 
  </body>
