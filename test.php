88b4ec8fa89458390f7f300fa1db6ce4545f47ff
sm65 <sm65@users.noreply.github.com>
2016-04-16 07:14:42 -0400

    Update test.php

--- test.php
+++ test.php
@@ -16,15 +16,13 @@
 
 <?php
 echo"test1 \n";
-$server = stream_socket_server('udp://127.0.0.1:5001, $errno, $errstr');
-if (!$server) {
+$socket = stream_socket_server("udp://127.0.0.1:1113", $errno, $errstr, STREAM_SERVER_BIND);
+if (!$socket) {
     die("$errstr ($errno)");
-    echo"failed \n";
 }
-echo "Data: '" . stream_socket_recvfrom($server, 14) . "'\n";
 
 echo"test2 \n";
-fclose($server);
+
 ?>
  </body>
 </html>
