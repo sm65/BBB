397c386a64b834c0cc37038e23e9b5226fd455ea
sm65 <sm65@users.noreply.github.com>
2016-04-17 04:37:51 -0400

    Update test.php

--- test.php
+++ test.php
@@ -24,7 +24,7 @@ if(!($sock = socket_create(AF_INET, SOCK_DGRAM,  SOL_UDP)))
 {
     $errorcode = socket_last_error();
     $errormsg = socket_strerror($errorcode);
-     
+    echo "Socket create failed \n"; 
     die("Couldn't create socket: [$errorcode] $errormsg \n");
 }
  
@@ -35,7 +35,7 @@ if( !socket_bind($sock, "0.0.0.0" , 5001) )
 {
     $errorcode = socket_last_error();
     $errormsg = socket_strerror($errorcode);
-     
+    echo "Socket bind failed \n";
     die("Could not bind socket : [$errorcode] $errormsg \n");
 }
  
@@ -47,11 +47,8 @@ while(1)
     echo "Waiting for data ... \n";
      
     //Receive some data
-    $r = socket_recvfrom($sock, $buf, 512, 0, $remote_ip, $remote_port);
+    $r = socket_recvfrom($sock, $buf, 14, 0, $remote_ip, $remote_port);
     echo "$remote_ip : $remote_port -- " . $buf;
-     
-    //Send back the data to the client
-    socket_sendto($sock, "OK " . $buf , 100 , 0 , $remote_ip , $remote_port);
 }
  
 socket_close($sock);
