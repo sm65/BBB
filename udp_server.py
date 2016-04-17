5c11790517ed5b45063e27c9c79e7d04214d90d7
sm65 <sm65@users.noreply.github.com>
2016-04-17 05:35:11 -0400

    Create udp_server.py

---
+++ udp_server.py
@@ -0,0 +1,12 @@
+import socket
+
+UDP_IP = "0.0.0.0"
+UDP_PORT = 5001
+
+sock = socket.socket(socket.AF_INET, # Internet
+                 socket.SOCK_DGRAM) # UDP
+sock.bind((UDP_IP, UDP_PORT))
+print "waiting"
+while True:
+    data, addr = sock.recvfrom(14) # buffer size is 1024 bytes
+    print "received message:", data, " address:", addr
