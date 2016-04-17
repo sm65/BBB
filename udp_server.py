0038dabb778cf4adbdc049a7cfd507b6daecb328
sm65 <sm65@users.noreply.github.com>
2016-04-17 08:20:47 -0400

    Update udp_server.py

--- udp_server.py
+++ udp_server.py
@@ -9,8 +9,8 @@ sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 sock.bind((UDP_IP, UDP_PORT))
 print "waiting"
 try:
-while True:
-    data, addr = sock.recvfrom(14) # buffer size is 1024 bytes
-    print "received message:", data, " address:", addr
+    while True:
+        data, addr = sock.recvfrom(14) # buffer size is 1024 bytes
+        print "received message:", data, " address:", addr
 except KeyboardInterrupt:
     pass
