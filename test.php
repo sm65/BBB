217b03f2ba8a9a623858b7240328c5e636e3ced7
sm65 <sm65@users.noreply.github.com>
2016-04-19 04:51:16 -0400

    Update test.php

--- test.php
+++ test.php
@@ -5,6 +5,7 @@
 <meta name="description" content="" />
 <meta name="keywords" content="" />
 <meta name="author" content="" />
+<meta http-equiv="refresh" content="5" />
 <link rel="stylesheet" type="text/css" href="style.css" media="screen" />
 <title>Test Page</title>
  <body>
