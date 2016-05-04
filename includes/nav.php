987a9749fbba257fc3269d85e056aebc41104c57
sm65 <sm65@users.noreply.github.com>
2016-05-04 05:01:31 -0400

    Update nav.php

--- includes/nav.php
+++ includes/nav.php
@@ -3,7 +3,7 @@
 	<a href="index.php">Home</a>
 	<a href="graph.php">O2 graph</a>
 	<a href="O2.php#">O2 Log</a>
-	<a href="about.php">BBB Stuff</a>
-	<a href="test.php">udp test</a>
+	<a href="about.php">BeagleBoneBlack</a>
+	<a href="test.php">CC3100 WiFi</a>
 	<!-- <a href="chart.php">Google Chart</a> -->
 </div> <!-- end #nav -->
