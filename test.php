17f1edff03d858754db568cb68aa448a40fcef89
sm65 <sm65@users.noreply.github.com>
2016-06-04 16:25:19 -0400

    Update test.php

--- test.php
+++ test.php
@@ -1,10 +1,8 @@
+<!--
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
 <style type="text/css">
-<!--
-.tab { margin-left: 40px; }
--->
 </style>	
 <meta http-equiv="content-type" content="text/html; charset=utf-8" />
 <meta name="description" content="" />
@@ -12,6 +10,8 @@
 <meta name="author" content="" />
 <meta http-equiv="refresh" content="5" />
 <link rel="stylesheet" type="text/css" href="style.css" media="screen" />
+-->
+
 <title>WiFI</title>
  <body>
  	<?php include('includes/header.php');
