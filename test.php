1dfbedc17ddaa152e0dbcbc348f13202c74acf02
sm65 <sm65@users.noreply.github.com>
2016-06-04 16:39:39 -0400

    Update test.php

--- test.php
+++ test.php
@@ -1,16 +1,4 @@
-<!--
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-<head>
-<style type="text/css">
-</style>	
-<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-<meta name="description" content="" />
-<meta name="keywords" content="" />
-<meta name="author" content="" />
-<meta http-equiv="refresh" content="5" />
-<link rel="stylesheet" type="text/css" href="style.css" media="screen" />
--->
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
@@ -22,10 +10,11 @@
 <title>WiFI</title>
 </head>
  <body>
+ 	<div id="wrapper">
  	<?php include('includes/header.php');
 	include('includes/nav.php');
-	include('includes/sidebar.php');
 	?>
+  <div id="content">	
   <h1>UDP server test</h1>
   <p class="tab">Remote WiFi UDP client:<br></br>
   <?php
@@ -53,5 +42,9 @@
   }
   ?>
   </p>
+  </div> <!-- end #content -->
+<?php include('includes/sidebar.php'); ?>
+<?php include('includes/footer.php'); ?>
+</div> <!-- End #wrapper -->
  </body>
 </html>
