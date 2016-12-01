4036796c2b5988bd2046394b6bc1dcdb562f3d74
sm65 <sm65@users.noreply.github.com>
2016-12-01 05:19:49 -0500

    Update udp_server.py

--- udp_server.py
+++ udp_server.py
@@ -11,14 +11,13 @@ sock = socket.socket(socket.AF_INET, # Internet
                  socket.SOCK_DGRAM) # UDP
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)                
 sock.bind((UDP_IP, UDP_PORT))
+sock.settimeout(2)
 print "waiting"
-#try:
-    #while True:
-data, addr = sock.recvfrom(25) # buffer size is 1024 bytes
+try:
+    data, addr = sock.recvfrom(25) # buffer size is 1024 bytes
         #print "received message:", data, " address:", addr
-#except KeyboardInterrupt:
-    #pass
-sock.close()
+except socket.timeout:
+    sock.close()
 print_data = str(datenow) + ', ' + data[15:20]
 print print_data
 with open('/var/www/temp/Log.csv', 'a') as f:
