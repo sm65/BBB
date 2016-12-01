307acdde3345f331f0b2289cb7c79d07cf5d00f9
sm65 <sm65@users.noreply.github.com>
2016-12-01 04:36:38 -0500

    Update udp_server.py

--- udp_server.py
+++ udp_server.py
@@ -1,4 +1,8 @@
 import socket
+import subprocess
+d = subprocess.Popen("date", stdout=subprocess.PIPE, shell=True)
+(datenow, err) = d.communicate()
+datenow = datenow.strip()
 
 UDP_IP = "0.0.0.0"
 UDP_PORT = 5001
@@ -10,8 +14,14 @@ sock.bind((UDP_IP, UDP_PORT))
 print "waiting"
 try:
     while True:
-        data, addr = sock.recvfrom(14) # buffer size is 1024 bytes
+        data, addr = sock.recvfrom(25) # buffer size is 1024 bytes
         print "received message:", data, " address:", addr
 except KeyboardInterrupt:
     pass
     sock.close()
+print_data = str(datenow) + ', ' + data
+print print_data
+with open('/var/www/temp/Log.csv', 'a') as f:
+        f.write(print_data)
+f.closed   
+    
