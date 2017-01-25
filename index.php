d58cd991d033f6e58aaef0e88b417a47ee0c23b8
sm65 <sm65@users.noreply.github.com>
2017-01-25 04:25:58 -0500

    Update index.php

--- index.php
+++ index.php
@@ -15,6 +15,7 @@
 <?php include('includes/nav.php'); ?>
 <?php include('includes/sidebar.php'); ?>			
 <iframe width="250" height="750" class="rssdog" src="http://www.rssdog.com/index.htm?url=http%3A%2F%2Ffeeds.reuters.com%2FReuters%2FworldNews&mode=html&showonly=&maxitems=0&showdescs=0&desctrim=0&descmax=0&tabwidth=100%25&linktarget=_blank&bordercol=%23d4d0c8&headbgcol=%23999999&headtxtcol=%23ffffff&titlebgcol=%23f1eded&titletxtcol=%23000000&itembgcol=%23ffffff&itemtxtcol=%23000000&ctl=0"></iframe>
+<iframe width="250" height="750" class="rssdog" src="http://www.rssdog.com/index.htm?url=http%3A%2F%2Ffeeds.reuters.com%2FReuters%2FdomesticNews&mode=html&showonly=&maxitems=0&showdescs=0&desctrim=0&descmax=0&tabwidth=100%25&linktarget=_blank&bordercol=%23d4d0c8&headbgcol=%23999999&headtxtcol=%23ffffff&titlebgcol=%23f1eded&titletxtcol=%23000000&itembgcol=%23ffffff&itemtxtcol=%23000000&ctl=0"></iframe>		
 		</div> <!-- End #wrapper -->
 	</body>
 </html>
