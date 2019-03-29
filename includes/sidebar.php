6f1aa6c52cd44c556c80f7d4275f87560502d17a
sm65 <sm65@users.noreply.github.com>
2019-03-29 04:55:46 -0400

    Update sidebar.php

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -4,11 +4,9 @@
         <li><a href="http://www.wunderground.com/US/NC/Indian_Trail.html?extendedsun=sunon&MR=1" target="_self">WunderGround IT</a></li>
         <li><a href="http://rd.excite.com/sp/em">Email</a></li>
         <li><a href="http://www.tvguide.com/listings/">TV</a></li>
-        <li><a href="https://my.yahoo.com/">Yahoo</a></li>
+        <li><a href="https://www.youtube.com/">You Tube</a></li>
         <li><a href="http://www.excite.com/">Excite</a></li>
         <li><a href="http://www.spacedaily.com/">Spacedaily</a></li>
-	<li><a href="http://beagleboard.org/Products/BeagleBone%20Black">BBB Mfg. page</a></li>
-	<li><a href="http://elinux.org/BeagleBoardUbuntu#eMMC:_BeagleBone_Black">BBB eLinux page</a></li>
 	<li><a href="https://github.com/sm65/BBB">Source code page</a></li>
 <p></p>
 <div style="margin: 15px 0px 0px; display: inline-block; text-align: center; width: 118px;"><div style="display: inline-block; padding: 2px 4px; margin: 0px 0px 5px; border: 1px solid rgb(204, 204, 204); text-align: center; background-color: rgb(255, 255, 255);"><a style="text-decoration: none; font-size: 13px; color: rgb(0, 0, 0);" href="http://localtimes.info/North_America/United_States/North_Carolina/Indian_Trail/"><img src="http://localtimes.info/images/countries/us.png"="" border=0="" style="border:0;margin:0;padding:0"=""> Indian+Trail</a></div><script src="http://localtimes.info/clock.php?continent=North America&country=United States&province=North Carolina&city=Indian Trail&cp1_Hex=000000&cp2_Hex=FFFFFF&cp3_Hex=000000&fwdt=118&ham=0&hbg=0&hfg=0&sid=0&mon=0&wek=0&wkf=0&sep=0&widget_number=117" type="text/javascript"></script></div>
