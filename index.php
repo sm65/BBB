91662b3da35c5bbacb2dcbb1bf0ed93fd80a9c97
sm65 <sm65@users.noreply.github.com>
2019-11-19 05:20:28 -0500

    test iframe

--- index.php
+++ index.php
@@ -15,6 +15,7 @@
 <?php include('includes/nav.php'); ?>
 <?php include('includes/sidebar.php'); ?>	
 <!-- start sw-rss-feed code --> 
+<iframe			
 <script type="text/javascript"> 
 <!-- 
 rssfeed_url = new Array(); 
@@ -58,8 +59,9 @@ rssfeed_cache = "5e35f4ec904fb58d73850becf1d740d6";
 <script type="text/javascript" src="//feed.surfing-waves.com/js/rss-feed.js"></script> 
 <!-- The link below helps keep this service FREE, and helps other people find the SW widget. Please be cool and keep it! Thanks. --> 
 <div style="color:#ccc;font-size:10px; text-align:right; width:250px;">powered by <a href="https://surfing-waves.com" rel="noopener" target="_blank" style="color:#ccc;">Surfing Waves</a></div> 
-<!-- end sw-rss-feed code -->			
+<!-- end sw-rss-feed code -->	</iframe>		
 <!-- start sw-rss-feed code --> 
+<iframe
 <script type="text/javascript"> 
 <!-- 
 rssfeed_url = new Array(); 
@@ -104,6 +106,7 @@ rssfeed_cache = "5e35f4ec904fb58d73850becf1d740d6";
 <!-- The link below helps keep this service FREE, and helps other people find the SW widget. Please be cool and keep it! Thanks. --> 
 <div style="color:#ccc;font-size:10px; text-align:right; width:250px;">powered by <a href="https://surfing-waves.com" rel="noopener" target="_blank" style="color:#ccc;">Surfing Waves</a></div> 
 <!-- end sw-rss-feed code -->
+</iframe>
 		</div> <!-- End #wrapper -->
 	</body>
 </html>
