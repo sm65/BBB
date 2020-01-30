e371cfe5d36917f0112bb286e25f67eee05eb678
sm65 <sm65@users.noreply.github.com>
2020-01-30 05:13:22 -0500

    Update sidebar.php

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -8,5 +8,4 @@
         <li><a href="http://www.spacedaily.com/">Spacedaily</a></li>
 	<li><a href="https://github.com/sm65/BBB">Source code page</a></li>
 <p></p>
-<div style="margin: 15px 0px 0px; display: inline-block; text-align: center; width: 118px;"><div style="display: inline-block; padding: 2px 4px; margin: 0px 0px 5px; border: 1px solid rgb(204, 204, 204); text-align: center; background-color: rgb(255, 255, 255);"><a style="text-decoration: none; font-size: 13px; color: rgb(0, 0, 0);"</div>
 </div> <!-- end #sidebar -->
