7907a459df1a0af60c434b7c26e194b0d9ba51f3
sm65 <sm65@users.noreply.github.com>
2020-04-23 04:38:34 -0400

    Update sidebar.php

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,7 +1,7 @@
 <div id="sidebar">
 
 <h3>Links</h3>
-        <li><a href="https://www.wunderground.com/weather/us/nc/matthews/KNCMATTH53">WunderGround IT</a></li>
+        <li><a href="https://www.wunderground.com/weather/us/nc/indian-trail/KNCINDIA40">WunderGround IT</a></li>
         <li><a href="https://login.excite.com/">Email</a></li>
         <li><a href="http://www.tvguide.com/listings/">TV</a></li>
         <li><a href="https://www.youtube.com/">You Tube</a></li>
