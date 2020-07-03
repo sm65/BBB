f14bcfd228543234a8752de29ef9f24a2bba1a89
sm65 <sm65@users.noreply.github.com>
2020-07-03 03:17:45 -0400

    Update o2.py

--- O2/o2.py
+++ O2/o2.py
@@ -1,13 +1,13 @@
-import smbus
+#import smbus
 import subprocess
 d = subprocess.Popen("date", stdout=subprocess.PIPE, shell=True)
 (datenow, err) = d.communicate()
 datenow = datenow.strip()
 #print datenow
-bus = smbus.SMBus(1)
-rawtemp=bus.read_word_data(0x40, 1)
-degc = ((rawtemp % 256)*256 + (rawtemp / 256))/128.0
-degf = round(degc*1.8+20,1)
+#bus = smbus.SMBus(1)
+#rawtemp=bus.read_word_data(0x40, 1)
+#degc = ((rawtemp % 256)*256 + (rawtemp / 256))/128.0
+#degf = round(degc*1.8+20,1)
 #print degf
 #print degc
 with open('/sys/devices/ocp.3/44e0d000.tscadc/tiadc/iio:device0/in_voltage4_raw', 'r') as f:
@@ -20,8 +20,8 @@ with open('/var/www/temp/spanAtoD.csv', 'r') as f:
 zeroAtoD=400
 calfactor=20.9/(int(spanAtoD)-zeroAtoD)
 O2 =round( (int(read_data)-zeroAtoD)*calfactor,2)
-
-print_data = str(datenow) + ', ' + str(degf)+ ',' + ' DegF' + ', ' + str(O2) + ',' + ' %O2\n'
+print_data = str(datenow) + ', ' + str(O2) + ',' + ' %O2\n'
+#print_data = str(datenow) + ', ' + str(degf)+ ',' + ' DegF' + ', ' + str(O2) + ',' + ' %O2\n'
 #print read_data
 with open('/var/www/temp/Log.csv', 'a') as f:
         f.write(print_data)
