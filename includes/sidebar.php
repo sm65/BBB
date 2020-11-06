d40d62873066a918c115fd59f17f7ee7432bfc2a
sm65 <sm65@users.noreply.github.com>
2020-11-06 04:26:52 -0500

    Update sidebar.php

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -3,7 +3,7 @@
 <h3>Links</h3>
         <li><a href="https://www.wunderground.com/weather/us/nc/indian-trail/KNCINDIA40">WunderGround IT</a></li>
         <li><a href="https://login.excite.com/">Email</a></li>
-        <li><a href="http://www.tvguide.com/listings/">TV</a></li>
+        <li><a href="https://titantv.com/default.aspx/">TV</a></li>
         <li><a href="https://www.youtube.com/">You Tube</a></li>
         <li><a href="http://www.spacedaily.com/">Spacedaily</a></li>
 	<li><a href="https://github.com/sm65/BBB">Source code page</a></li>
