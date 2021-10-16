61f8ffca1e3c111aedf362733f3a098fcfeb508e
sm65 <sm65@users.noreply.github.com>
2021-10-16 04:44:52 -0400

    Update sidebar.php

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -3,7 +3,7 @@
 <h3>Links</h3>
         <li><a href="https://www.wunderground.com/weather/us/nc/indian-trail/KNCINDIA40">WunderGround IT</a></li>
         <li><a href="https://mail.protonmail.com/">Email</a></li>
-        <li><a href="https://titantv.com/default.aspx/">TV</a></li>
+        <li><a href="https://www.tvpassport.com/tv-listings#">TV</a></li>
         <li><a href="https://www.youtube.com/">You Tube</a></li>
         <li><a href="http://www.spacedaily.com/">Spacedaily</a></li>
 	<li><a href="https://github.com/sm65/BBB">Source code page</a></li>
