d1d85168880c03c97cf949ea033f053f4b3772e2
sm65 <sm65@users.noreply.github.com>
2022-11-05 04:30:33 -0400

    Update sidebar.php

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -2,7 +2,7 @@
 
 <h3>Links</h3>
         <li><a href="https://www.wunderground.com/weather/us/nc/indian-trail/KNCINDIA40">WunderGround IT</a></li>
-        <li><a href="https://mail.protonmail.me/">Email</a></li>
+        <li><a href="https://mail.proton.me/u/0/inbox">Email</a></li>
         <li><a href="https://www.tvpassport.com/tv-listings#">TV</a></li>
         <li><a href="https://www.youtube.com/">You Tube</a></li>
         <li><a href="http://www.spacedaily.com/">Spacedaily</a></li>
