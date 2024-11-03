5e771a4f3b0af4f2f991229035d53989d54ae1ea
sm65 <sm65@users.noreply.github.com>
2024-11-03 05:09:57 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+verge<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
 <meta http-equiv="content-type" content="text/html; charset=utf-8" />
@@ -16,7 +16,7 @@
 <?php include('includes/sidebar.php'); ?>	
 <iframe src="http://www.smealy.com/getrss.php?time=1593204233371&amp;x=http%3A%2F%2Ffeeds.bbci.co.uk%2Fnews%2Fworld%2Frss.xml&amp;w=250&amp;h=750&amp;bc=333333&amp;bw=1&amp;bgc=transparent&amp;m=20&amp;it=false&amp;t=(default)&amp;tc=333333&amp;ts=15&amp;tb=transparent&amp;il=true&amp;lc=0000FF&amp;ls=14&amp;lb=false&amp;id=false&amp;dc=333333&amp;ds=14&amp;idt=false&amp;dtc=284F2D&amp;dts=12" border="0" hspace="0" vspace="0" marginwidth="0" marginheight="0" style="border:0; padding:0; margin:0; width:250px; height:750px;" id="rssOutput" frameborder="no">Reading RSS Feed ...</iframe>
 <iframe src="http://www.smealy.com/getrss.php?time=1593461802050&amp;x=http%3A%2F%2Ffeeds.mashable.com%2Fmashable&amp;w=250&amp;h=750&amp;bc=333333&amp;bw=1&amp;bgc=transparent&amp;m=20&amp;it=false&amp;t=(default)&amp;tc=333333&amp;ts=15&amp;tb=transparent&amp;il=true&amp;lc=0000FF&amp;ls=14&amp;lb=false&amp;id=false&amp;dc=333333&amp;ds=14&amp;idt=false&amp;dtc=284F2D&amp;dts=12" border="0" hspace="0" vspace="0" marginwidth="0" marginheight="0" style="border:0; padding:0; margin:0; width:250px; height:750px;" id="rssOutput" frameborder="no">Reading RSS Feed ...</iframe>
-<iframe src="http://www.smealy.com/getrss.php?time=1593204233371&amp;x=http%3A%2F%2Ftechcrunch.com%2Ffeed&amp;w=250&amp;h=750&amp;bc=333333&amp;bw=1&amp;bgc=transparent&amp;m=20&amp;it=false&amp;t=(default)&amp;tc=333333&amp;ts=15&amp;tb=transparent&amp;il=true&amp;lc=0000FF&amp;ls=14&amp;lb=false&amp;id=false&amp;dc=333333&amp;ds=14&amp;idt=false&amp;dtc=284F2D&amp;dts=12" border="0" hspace="0" vspace="0" marginwidth="0" marginheight="0" style="border:0; padding:0; margin:0; width:250px; height:750px;" id="rssOutput" frameborder="no">Reading RSS Feed ...</iframe>			
+<iframe src="http://www.smealy.com/getrss.php?time=1593204233371&amp;x=http%3A%2F%2Ftheverge.com%2Frss%2Ffrontpage&amp;w=250&amp;h=750&amp;bc=333333&amp;bw=1&amp;bgc=transparent&amp;m=20&amp;it=false&amp;t=(default)&amp;tc=333333&amp;ts=15&amp;tb=transparent&amp;il=true&amp;lc=0000FF&amp;ls=14&amp;lb=false&amp;id=false&amp;dc=333333&amp;ds=14&amp;idt=false&amp;dtc=284F2D&amp;dts=12" border="0" hspace="0" vspace="0" marginwidth="0" marginheight="0" style="border:0; padding:0; margin:0; width:250px; height:750px;" id="rssOutput" frameborder="no">Reading RSS Feed ...</iframe>			
 			</div><!--  End #wrapper -->
 	</body>
 </html>		
