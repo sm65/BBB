f895da3b8aae627bbf4dfd77fb285a588d908467
sm65 <sm65@users.noreply.github.com>
2025-08-15 15:05:21 -0400

    Update sidebar.php

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,7 +1,7 @@
 <div id="sidebar">
 
 <h3>Links</h3>
-        <li><a href="https://www.wunderground.com/weather/us/nc/indian-trail/KNCINDIA112">WunderGround IT</a></li>
+        <li><a href="https://www.wunderground.com/weather/us/nc/indian-trail/KNCINDIA127">WunderGround IT</a></li>
         <li><a href="https://mail.proton.me/u/0/inbox">Email</a></li>
         <li><a href="https://www.tvpassport.com/tv-listings#">TV</a></li>
         <li><a href="https://www.youtube.com/">You Tube</a></li>
@@ -10,4 +10,5 @@
 	<li><a href="https://apod.nasa.gov/apod/">NASA APOD</a></li>
 <p></p>
 </div> <!-- end #sidebar -->
+
 
